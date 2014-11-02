7a5e8344784420d3be06b74c3512e8b64c099f2d
Vishnu Prem <vishnu@vishnuprem.com>
2014-11-03 07:56:48 +0800

    Initial Commit

---
+++ libclang experiments/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  libclang experiments
+//
+//  Created by Vishnu Prem on 3/11/14.
+//  Copyright (c) 2014 Vishnu Prem. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
